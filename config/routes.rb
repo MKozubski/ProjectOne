d7943cd76f3742c903ee9a6ad181adb8f5eeef20
MKozubski <mkozubski@aceproject.space>
2023-09-15 10:47:18 -0500

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'about/index'
   resources :breeds, only: [:index]
 
   root 'breeds#index'
+
+  get '/about', to: 'about#index'
 
   resources :breed_images, only: [:index, :show] do
     collection do
