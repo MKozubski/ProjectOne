765dc5c903bd55c82c34cdd377b5c497df7fe19c
jaspreet kaur <jgill11@academic.rrc.ca>
2023-10-04 22:33:44 -0400

    Added the validation for the breed search with a new file "_search_form/html.erb.

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,4 +1,5 @@
 class Breed < ApplicationRecord
   has_many :sub_breeds, dependent: :destroy
   has_many :breed_images
+  validates :name, presence: true, uniqueness: true,  length: { in: 3..15 }
 end
