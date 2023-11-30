3d2e9a27e103447a5a4f3549f4d4c1e2fba663e8
MKozubski <mkozubski@aceproject.space>
2023-11-30 12:05:38 -0600

    refactored search controller

--- app/controllers/hierarchical_search_controller.rb
+++ app/controllers/hierarchical_search_controller.rb
@@ -1,9 +1,15 @@
 class HierarchicalSearchController < ApplicationController
-    def search
-      sub_breeds_count = params[:sub_breeds_count].to_i
-      @search_performed = true
-      @breeds = Breed.includes(:sub_breeds).select { |breed| breed.sub_breeds.count == sub_breeds_count }
-      @breeds_count = sub_breeds_count
+  def search
+    sub_breeds_count = params[:sub_breeds_count].to_i
+    @search_performed = true
+
+    if sub_breeds_count.positive?
+      @breeds = Breed.joins(:sub_breeds).group('breeds.id').having('COUNT(sub_breeds.id) = ?', sub_breeds_count)
+    else
+      @breeds = []
+      flash.now[:alert] = 'Invalid sub breeds count'
     end
+
+    @breeds_count = sub_breeds_count
   end
-  
+end
